2a4b58e202c29867f53a67875997197430854839
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2024-08-31 14:50:01 +0800

    initialize

---
+++ src/pyasyncproxy/__main__.py
@@ -0,0 +1,12 @@
+"""The asynchronous rpc application.
+
+Copyright (c) 2023-present 善假于PC也 (zlhywlf).
+"""
+
+
+def main() -> None:
+    """The asynchronous network proxy application."""
+
+
+if __name__ == "__main__":
+    main()
