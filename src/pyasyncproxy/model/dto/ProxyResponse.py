595a835109187eec47e43a7a04828b85f3ffad12
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2024-09-11 17:49:03 +0800

    optimize

--- src/pyasyncproxy/model/dto/ProxyResponse.py
+++ src/pyasyncproxy/model/dto/ProxyResponse.py
@@ -5,7 +5,7 @@ Copyright (c) 2023-present 善假于PC也 (zlhywlf).
 
 from collections.abc import Mapping
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class ProxyResponse(BaseModel):
@@ -14,4 +14,4 @@ class ProxyResponse(BaseModel):
     code: int
     headers: Mapping[str, str] | None = None
     media_type: str | None
-    content: bytes
+    content: bytes = Field(..., repr=False)
